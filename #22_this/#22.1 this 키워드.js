af93342732281ca2857a8bd68991df273e3895d1
Min <esspacetoM@gmail.com>
2023-07-13 17:26:40 +0900

    22장 this

---
+++ #22_this/#22.1 this 키워드.js
@@ -0,0 +1,85 @@
+// 22-1
+const circle = {
+  // 프로퍼티: 객체 고유의 상태 데이터
+  radius: 5,
+  // 메서드: 상태 데이터를 참조하고 조작하는 동작
+  getDiameter() {
+    // 이 메서드가 자신이 속한 객체의 프로퍼티나 다른 메서드를 참조하려면
+    // 자신이 속한 객체인 circle을 참조할 수 있어야 한다.
+    return 2 * circle.radius;
+  }
+};
+
+console.log(circle.getDiameter()); // 10
+
+// 22-2
+function Circle(radius) {
+  // 이 시점에는 생성자 함수 자신이 생성할 인스턴스를 가리키는 식별자를 알 수 없다.
+  ????.radius = radius;
+}
+
+Circle.prototype.getDiameter = function () {
+  // 이 시점에는 생성자 함수 자신이 생성할 인스턴스를 가리키는 식별자를 알 수 없다.
+  return 2 * ????.radius;
+};
+
+// 생성자 함수로 인스턴스를 생성하려면 먼저 생성자 함수를 정의해야 한다.
+const circle = new Circle(5);
+
+// 22-3
+// 객체 리터럴
+const circle = {
+  radius: 5,
+  getDiameter() {
+    // this는 메서드를 호출한 객체를 가리킨다.
+    return 2 * this.radius;
+  }
+};
+
+console.log(circle.getDiameter()); // 10
+
+// 22-4
+// 생성자 함수
+function Circle(radius) {
+  // this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+  this.radius = radius;
+}
+
+Circle.prototype.getDiameter = function () {
+  // this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+  return 2 * this.radius;
+};
+
+// 인스턴스 생성
+const circle = new Circle(5);
+console.log(circle.getDiameter()); // 10
+
+// 22-5
+// this는 어디서든지 참조 가능하다.
+// 전역에서 this는 전역 객체 window를 가리킨다.
+console.log(this); // window
+
+function square(number) {
+  // 일반 함수 내부에서 this는 전역 객체 window를 가리킨다.
+  console.log(this); // window
+  return number * number;
+}
+square(2);
+
+const person = {
+  name: 'Lee',
+  getName() {
+    // 메서드 내부에서 this는 메서드를 호출한 객체를 가리킨다.
+    console.log(this); // {name: "Lee", getName: ƒ}
+    return this.name;
+  }
+};
+console.log(person.getName()); // Lee
+
+function Person(name) {
+  this.name = name;
+  // 생성자 함수 내부에서 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+  console.log(this); // Person {name: "Lee"}
+}
+
+const me = new Person('Lee');
